4c950d167dc88eef889df5f07da837f27711ba3c
Blake Parrish <bwparrish@gmail.com>
2023-12-27 09:08:05 -0500

    input info and beginnings of the solution

---
+++ day_2/rock_paper_scissor.py
@@ -0,0 +1,10 @@
+def point_total(file_path):
+
+    with open(file_path) as file:
+        a = x = 1
+        b = y = 2
+        c = z = 3
+        for line in file:
+            if line 
+
+point_total('/home/blake/aoc_2022/day_2/input.txt')
