6cf5284eacc1e17e42bb537f76b127465183cc61
Blake Parrish <bwparrish@gmail.com>
2023-12-27 07:32:18 -0500

    final answer

--- day_1/puzzle_1.py
+++ day_1/puzzle_1.py
@@ -1,8 +1,14 @@
 #!/bin/python3
 
+def solution(file_path):
+    with open(file_path, 'rt') as file:
+        meals = file.read().split("\n\n")
+    
+    elf_calories = []
+    for elf in meals:
+        calories = sum(map(int,elf.splitlines()))
+        elf_calories.append(calories)
+        print(max(elf_calories))
 
-with open('/home/blake/aoc_2022/day_1/input.txt', 'r') as file:
-    for line in file:
-        elves = line.strip().split()
-        print(elves)
+solution('/home/blake/aoc_2022/day_1/input.txt')
 
