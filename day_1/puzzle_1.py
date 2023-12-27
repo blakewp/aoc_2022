1bd658d3db151de65467901f917a920dcde7bbb7
Blake Parrish <bwparrish@gmail.com>
2023-12-26 20:25:19 -0500

    first commit

---
+++ day_1/puzzle_1.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import codecs
+
+f=codecs.open("input.txt", 'r')
+
+while 
+for numbers in f:
+    number = int(numbers)
+    if number >= 1:
+        total = sum(number)
+        print(total)
